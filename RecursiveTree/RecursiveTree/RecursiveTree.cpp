54d8779baac68eb0d4bc404c57298d24a19258d7
Kuznecov Fedr <pasasi@gmail.com>
2020-03-25 22:19:23 +0400

    this is recursive tree for Galanin

---
+++ RecursiveTree/RecursiveTree/RecursiveTree.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "MyTree.h"
+using namespace std;
+
+
+int main()
+{
+    MyTree<int> tree;
+    int a[] = { 5,3,1,2,7,6,8 };
+    for (int i = 0; i < 7; i++)
+    {
+        tree.AddElement(a[i]);
+    }
+    tree.PrintAll();
+    cout << (tree.Find(1) ? "We have -1" : "We dont have -1(") << endl;
+
+    tree.DeleteTree();
+}
