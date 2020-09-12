4e6af19ccac7168ef5586ccbed675c4de519a4b2
Fedr <fedr@DESKTOP-VO2O000.Dlink>
2020-09-12 18:23:18 +0000

    add snakeFill and strangePrint

--- For Konstantin Sergeevich/CSharp/CSharp/MatrixUtils.cs
+++ For Konstantin Sergeevich/CSharp/CSharp/MatrixUtils.cs
@@ -15,10 +15,61 @@ namespace CSharp
             base(rawMatrix)
         { }
 
-        public void snakeFilling()
-        {
-
-        }
+        public void snakeFill()
+	{
+        
+        	int maxCircle = Math.Min(mtrx.Length, mtrx[0].Length) / 2;
+        	for (int circle = 0 ; circle < maxCircle ; ++circle)
+        	{
+            		for (; i < rows - circle ; ++i )
+            		{
+                		base[i, j] = counter;
+                		++counter;
+            		}
+            		--i;
+            		++j;
+            		for (; j < cols - circle ; ++j )
+            		{	
+                		base[i, j] = counter;
+                		++counter;
+            		}
+            		--j;
+            		--i;
+            		for (; i >= circle ; --i)
+            		{
+                		base[i, j] = counter;
+                		++counter;
+            		}
+            		++i;
+            		--j;
+            		for (; j > circle ; --j )
+            		{
+                		base[i, j] = counter;
+                		++counter;
+            		}
+            		++i;
+            		++j;
+        	}
+	}
+	
+	static void printStrange()
+	{
+        	int max = rows / 2;  
+        	for (int i = 0; i < max ; ++i )
+        	{
+            		for (int j = 0 ; j < rows ; ++j)
+            		{
+                		Console.Write(base[j, i] + " " + base[rows - j - 1, cols - i - 1] + " ");
+            		}
+            		Console.Write('\n');
+        	}
+        	if (rows % 2 == 1 )
+        	{
+            		for (int i = 0 ; i < cols ; ++i)
+                		Console.Write(base[rows/2 + 1, i]);
+        	}
+            
+    	}
 
         public void transposition()
         {
