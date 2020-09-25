5d22530d9196f89364ceecbc08279c1f7eaee8df
Kuznecov Fedr <pasasi@gmail.com>
2020-09-25 18:17:36 +0400

    add Tasks and TestWPF

---
+++ For Konstantin Sergeevich/Tasks/Tasks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        public static Semaphore sem = new Semaphore(4, 4);
+        static void Main(string[] args)
+        {
+            int qttyWorkers = 10;
+            Task[] workers = new Task[qttyWorkers];
+            for (int i = 0; i < workers.Length; ++i)
+            {
+                workers[i] = Task.Run(Work);
+            }
+
+            Task.WaitAll(workers);
+        }
+
+        static public void Work()
+        {
+            sem.WaitOne();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} is in the Work!");
+            Thread.Sleep(1000);
+            timer.Stop();
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} end its Work and do shit about {timer.ElapsedMilliseconds * 1.0/1000} sec!");
+            sem.Release();
+        }
+    }
+}
---
+++ For Konstantin Sergeevich/TestWPF/TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String leftOp;
+        private String rightOp;
+        private String operation;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in LayoutRoot.Children )
+            {
+                if ( el is Button)
+                {
+                    ((Button)el).Click += buttonClick;
+                }
+            }
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            String s = (string)((Button)e.OriginalSource).Content;
+            textBlock.Text += s;
+            Int32 num;
+            bool result = Int32.TryParse(s, out num);
+            if (result)// if input is button
+            {
+                if (operation == "")
+                {
+                    leftOp += s;
+                }
+                else
+                {
+                    rightOp += s;
+                }
+            }
+            else // if clicked on +- or some this buttons
+            {
+                if (s == "=")
+                {
+                    updateRightOp();
+                    textBlock.Text = rightOp.ToString();
+                    operation = "";
+                }
+                else if (s == "CLEAR")
+                {
+                    leftOp = "";
+                    rightOp = "";
+                    operation = "";
+                    textBlock.Text = "";
+                }
+                else
+                {
+                    if (rightOp != "" )
+                    {
+                        updateRightOp();
+                        leftOp = rightOp;
+                        rightOp = "";
+                    }
+                    operation = s;
+                }
+            }
+        }
+
+        private void updateRightOp()
+        {
+            int num1 = 0;
+            int num2 = 0;
+            try 
+            {
+                num1 = Int32.Parse(leftOp);
+                num2 = Int32.Parse(rightOp);
+            } 
+            catch (Exception ex) 
+            {
+                textBlock.Text = "Fuck you!";
+            }
+
+            switch (operation)
+            {
+                case "+":
+                    rightOp = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    rightOp = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    rightOp = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    rightOp = (num1 / num2).ToString();
+                    break;
+            }
+        }
+    }
+}
